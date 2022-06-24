db7b57dcc242be3ccb42a081de96d2351b100a27
olgajuanne <olgajdango@gmail.com>
2022-06-24 00:09:13 -0300

    aula 3

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,6 +1,7 @@
 import { CheckCircle, Lock } from 'phosphor-react'
 import { isPast, format } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR' 
+import { Link } from 'react-router-dom'
 
 interface LessonProps {
   title: string;
@@ -16,34 +17,34 @@ export function Lesson(props: LessonProps) {
   })
 
   return (
-    <a href="#">
-      <span className="">
+    <Link to= {`/event/lesson/${props.slug}`} className="group">
+      <span className="text-gray-300">
         {availableDateFromatted}
         </span>
 
-      <div className="rounded border border-gray-500 p-4 mt-2">
+      <div className="rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500">
         <header className=" flex items-center justify-between">
           {isLessonAvailable ? (
             <span className="text-sm text-blue-500 font-medium flex items-center gap-2">
-            <CheckCircle size={20} />
-            Conteúdo liberado
+              <CheckCircle size={20} />
+              Conteúdo liberado
             </span>
           ) : (
             <span className="text-sm text-orange-500 font-medium flex items-center gap-2">
-            <Lock size={20} />
-            Em breve
+              <Lock size={20} />
+              Em breve
             </span>
           )}
           
           <span className="text-xs rounded py-[0.125rem] px-2 text-white border border-green-300 font-bold">
            {props.type === 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
-            </span>
+          </span>
         </header>
 
         <strong className="text-gray-200 mt-5 block">
           {props.title}
         </strong>
       </div>
-    </a>
+    </Link>
   )
 }
